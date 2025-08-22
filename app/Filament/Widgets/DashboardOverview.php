25df07f961fa7a1a3b55eb60d9352d916e6b9638
rchaconrv <hola.hack09@gmail.com>
2025-08-22 01:46:15 -0400

    Improvements - cards autoclick

--- app/Filament/Widgets/DashboardOverview.php
+++ app/Filament/Widgets/DashboardOverview.php
@@ -41,39 +41,57 @@ class DashboardOverview extends BaseWidget
             Stat::make('Departamentos', $departments)
                 ->description('Total de departamentos registrados')
                 ->descriptionIcon('heroicon-m-building-office')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.departments.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Usuarios', $users)
                 ->description('Total de usuarios registrados')
                 ->descriptionIcon('heroicon-m-users')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.users.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Medicamentos', $medications)
                 ->description('Total de medicamentos registrados')
                 ->descriptionIcon('heroicon-m-rectangle-stack')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.medications.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Entradas', $entries)
                 ->description('Registros de entrada')
                 ->descriptionIcon('heroicon-m-arrow-up-right')
-                ->color('primary'),
+                ->color('primary')
+                ->url(route('filament.admin.resources.medication-entries.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Salidas', $outputs)
                 ->description('Registros de salida')
                 ->descriptionIcon('heroicon-m-arrow-down-left')
-                ->color('warning'),
+                ->color('warning')
+                ->url(route('filament.admin.resources.medication-outputs.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Bajo stock (<=5)', $lowStock)
                 ->description('Medicamentos por agotarse')
                 ->descriptionIcon('heroicon-m-exclamation-triangle')
-                ->color('danger'),
+                ->color('danger')
+                ->url(route('filament.admin.resources.medications.index'))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Salidas hoy (Militares)', $todayMilitary)
                 ->description('Cantidad entregada a militares hoy')
                 ->descriptionIcon('heroicon-m-arrow-down-left')
-                ->color('info'),
+                ->color('info')
+                ->url(route('filament.admin.resources.medication-outputs.index', ['table[filters][patient_type]' => 'military']))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Salidas hoy (Civiles)', $todayCivilian)
                 ->description('Cantidad entregada a civiles hoy')
                 ->descriptionIcon('heroicon-m-arrow-down-left')
-                ->color('secondary'),
+                ->color('secondary')
+                ->url(route('filament.admin.resources.medication-outputs.index', ['table[filters][patient_type]' => 'civilian']))
+                ->extraAttributes(['class' => 'cursor-pointer']),
             Stat::make('Salidas hoy (Deptos.)', $todayDepartmental)
                 ->description('Cantidad entregada a departamentos hoy')
                 ->descriptionIcon('heroicon-m-arrow-down-left')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.medication-outputs.index', ['table[filters][patient_type]' => 'department']))
+                ->extraAttributes(['class' => 'cursor-pointer']),
         ];
     }
 
